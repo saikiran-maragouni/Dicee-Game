c056c10348a6ea9403ae74fdd53494e022600072
saikiran <100951913+saikiran-maragouni@users.noreply.github.com>
2022-05-23 22:45:42 +0530

    Dice game-responsive version

---
+++ index.js
@@ -0,0 +1,29 @@
+function myFunction() {
+  var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+  var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+  var image1 = "images/dice" + randomNumber1 + ".png";
+  var image2 = "images/dice" + randomNumber2 + ".png";
+
+  // document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+  // document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+  // document.querySelectorAll("img")[0].setAttribute("src", image1)
+  // document.querySelectorAll("img")[1].setAttribute("src", image2)
+
+
+  document.querySelector(".img1").setAttribute("src",image1)
+  document.querySelector(".img2").setAttribute("src",image2)
+
+// -------Changing Heading-------
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+
+  } else {
+    document.querySelector("h1").innerHTML = "! DRAW !";
+  }
+
+}
